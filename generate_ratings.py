a76a50ce5da36884742e8282864f1d2049a1f1b8
Bart von Meijenfeldt <b.m.vonmeijenfeldt@gmail.com>
2022-09-19 21:14:14 +0200

    sort rating output

--- generate_ratings.py
+++ generate_ratings.py
@@ -1,5 +1,6 @@
 import csv
 from datetime import date
+from operator import itemgetter
 
 from src.barto.barto import BartoRatings
 from src.barto.game_result import GameResult
@@ -17,8 +18,8 @@ def read_game_results(path: str) -> list[GameResult]:
 def _convert_row_to_single_game_result(header: list[str], row: list[str]) -> GameResult:
     row_dict = dict(zip(header, row))
 
-    team1 = [player for col_name, player in row_dict.items() if col_name.startswith('Team 1 Player')]
-    team2 = [player for col_name, player in row_dict.items() if col_name.startswith('Team 2 Player')]
+    team1 = [player for col_name, player in row_dict.items() if col_name.startswith('Team 1')]
+    team2 = [player for col_name, player in row_dict.items() if col_name.startswith('Team 2')]
     points_team1 = int(row_dict['Score Team 1'])
     points_team2 = int(row_dict['Score Team 2'])
     return GameResult(team1=team1, team2=team2, points_team1=points_team1, points_team2=points_team2)
@@ -32,14 +33,15 @@ def get_ratings(game_results: list[GameResult]) -> dict:
 
 def save_results(ratings: dict) -> None:
     filename = f'ratings_{date.today()}.csv'
+    rating_list = list(ratings.items())
+    rating_list.sort(key=itemgetter(1), reverse=True)
 
     with open(filename, 'w') as f:
         writer = csv.writer(f, delimiter=',')
         header = ['Player', 'Rating']
         writer.writerow(header)
 
-        for player, rating in ratings.items():
-            row = [player, rating]
+        for row in rating_list:
             writer.writerow(row)
 
 if __name__ == "__main__":
