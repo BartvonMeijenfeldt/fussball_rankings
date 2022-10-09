f5d5651647a6d3c5c1760ea6f413eb25f7062796
Bart von Meijenfeldt <b.m.vonmeijenfeldt@gmail.com>
2022-10-09 16:19:22 +0200

    upload_result bugfix

--- upload_results.sh
+++ upload_results.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-cd "$( dirname -- "${BASH_SOURCE[0]}" )"
-echo $(ls)
+cd $( dirname -- "${BASH_SOURCE[0]}" )
 git switch latest_ratings
 git pull
-source ~/miniconda3/bin/activate fussball_rankings
-python generate_ratings.py && git add -A && git commit -m "`date`" && git push --set-upstream origin latest_ratings
+python generate_ratings.py
+git add -A
+git commit -m "`date`"
+git push --set-upstream origin latest_ratings
+git switch main
