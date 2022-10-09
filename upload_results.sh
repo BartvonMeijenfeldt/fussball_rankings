b9b13d28edd8959098a355bbefd3b07114e98e43
Bart von Meijenfeldt <b.m.vonmeijenfeldt@gmail.com>
2022-10-09 14:44:20 +0200

    activate conda env

--- upload_results.sh
+++ upload_results.sh
@@ -1,4 +1,5 @@
 cd $( dirname -- "${BASH_SOURCE[0]}" )
 git switch latest_ratings
 git pull
+source ~/miniconda3/bin/activate fussball_rankings
 python generate_ratings.py && git add -A && git commit -m "`date`" && git push --set-upstream origin latest_ratings
