6e52b89d7ffcab1a3ca3780cc5d17424634167b8
Bart von Meijenfeldt <b.m.vonmeijenfeldt@gmail.com>
2022-10-09 14:13:49 +0200

    only commit if no failure

--- upload_results.sh
+++ upload_results.sh
@@ -1,7 +1,4 @@
 cd $( dirname -- "${BASH_SOURCE[0]}" )
 git switch latest_ratings
 git pull
-python generate_ratings.py
-git add -A
-git commit -m "`date`"
-git push --set-upstream origin latest_ratings
+python generate_ratings.py && git add -A && git commit -m "`date`" && git push --set-upstream origin latest_ratings
