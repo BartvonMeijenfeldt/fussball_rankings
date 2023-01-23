132458ea937e31e6cd14033939cbb5225eec3025
Bart von Meijenfeldt <b.m.vonmeijenfeldt@gmail.com>
2023-01-23 21:10:02 +0100

    update automatic uploads to main branch

--- upload_results.sh
+++ upload_results.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 cd $( dirname -- "${BASH_SOURCE[0]}" )
-git switch latest_ratings
 git pull
 source ~/miniconda3/bin/activate fussball_rankings
-python generate_ratings.py && git add -A && git commit -m "`date`" && git push --set-upstream origin latest_ratings
-git switch main
+python generate_ratings.py && git add -A && git commit -m "`date`" && git push --set-upstream origin main
